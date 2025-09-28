35d48a0bf2cac9f3531a0fb6eb7f9a0ae969d1b2
vamshi aditya <vamshiadityaofficial@gmail.com>
2025-09-27 20:00:11 -0400

    checking db hit

--- backend/src/db/mongoose.ts
+++ backend/src/db/mongoose.ts
@@ -6,7 +6,9 @@ export const connectToMongo = async () => {
       throw new Error("MONGO_URI environment variable is not defined");
     }
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.error(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(
+      `✅ MongoDB Connected: ${conn.connection.host}, DB: ${conn.connection.name}`
+    );
   } catch (error) {
     console.error("Error connecting to MongoDB:", (error as Error).message);
     process.exit(1);
